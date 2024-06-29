0da77ef120ff8b9be74358434b6b299733a25a03
Ura-sapfir <Ura-sapfir@HOME-PC>
2024-06-29 22:54:54 +0500

    sd

---
+++ Wpf 12 work/src/ToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_12_work.src
+{
+    public class ToDo
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public ToDo() { }   
+        public ToDo(string title, DateTime date, string description) {  this.Title = title; this.Date = date; this.Description = description; }
+
+
+
+    }
+}
